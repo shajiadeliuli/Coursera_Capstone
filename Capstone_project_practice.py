e24db14d178451a18efbcabb1d5a4da831f3c005
shajiadeliuli <zhangjingxiasu@163.com>
2020-02-07 11:06:21 +0800

    practice capstone project on coursera

---
+++ Capstone_project_practice.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This notebook is mainly used for the Capstone Project!
+
+# ### Practice
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
